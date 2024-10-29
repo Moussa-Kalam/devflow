b967b7a1a44aa949c4d39a908e483daafea113ce
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-10-29 18:18:00 +0200

    chore: set up fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-primary-500">
-    Welcome to the world of Next.js
-  </h1>
+  <>
+    <h1 className="text-3xl font-bold">Welcome to the world of Next.js</h1>
+    <p className="font-space-grotesk text-3xl font-bold">
+      Welcome to the world of Next.js
+    </p>
+  </>
 );
 
 export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,10 @@ const config: Config = {
       screens: {
         xs: "420px",
       },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
+      },
     },
   },
 };
