580be113bdabbf9386f42bba185f3ab7a3444eb8
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-11-02 21:27:36 +0200

    feat: create auth routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+
+import NavBar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <NavBar />
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import React from "react";
 
 import "./globals.css";
-import NavBar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -43,7 +42,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NavBar />
           {children}
         </ThemeProvider>
       </body>
